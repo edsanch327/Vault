61976721d06efdf637faf3484c01d49be18752f2
Edward Sanchez <edsanch@outlook.com>
2026-09-14 20:46:35 -0600

    Everything is finished. I believe I have everything correct.

---
+++ index.js
@@ -0,0 +1,10 @@
+let code1 
+code1 = 7+3;
+const code2 = 92/2 -6;
+const code3 = (32 % 3)+ 37;
+const message "The vault has been secured. The combination is:";
+const CodeA = code1 +"-"+code2 + "-" + code3;
+const codeB = '${code1}-${code2}-${code3}';
+console.log( message);
+console.log( CodeA)
+console.log(codeB)
